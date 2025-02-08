4df69ef145e39e69a780e9874808ce8dd0c028cd
Rajesh Thapa <okthatsall678@gmail.com>
2025-02-08 22:48:59 +0545

    completed

---
+++ project/about.php
@@ -0,0 +1,63 @@
+<?php
+
+include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>About</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<div class="heading">
+   <h3>About us</h3>
+   <p> <a href="home.php">Home</a> / About </p>
+</div>
+
+<section class="about">
+
+   <div class="flex">
+
+      <div class="image">
+         <img src="images/about-img.jpg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>Why choose us?</h3>
+         <p>From bestsellers to hidden gems, we offer a vast range of books across all genres—fiction, non-fiction, academic, self-help, and more!</p>
+         <p>Get the best deals on books with regular discounts, seasonal sales, and special membership offers.</p>
+         <a href="contact.php" class="btn">Contact us</a>
+      </div>
+
+   </div>
+
+</section>
+
+<?php include 'footer.php'; ?>
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
